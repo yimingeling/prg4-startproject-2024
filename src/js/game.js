17b9c21ae831fcba47b62b2ebfb9e1c3a2e87429
KokoDoko <benni_jolink@hotmail.com>
2024-05-10 12:51:06 +0200

    improve startsettings

--- src/js/game.js
+++ src/js/game.js
@@ -1,11 +1,16 @@
 import '../css/style.css'
-import { Actor, Engine, Vector } from "excalibur"
+import { Actor, Engine, Vector, DisplayMode } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
 
 export class Game extends Engine {
 
     constructor() {
-        super({ width: 800, height: 600 })
+        super({ 
+            width: 1280,
+            height: 720,
+            maxFps: 60,
+            displayMode: DisplayMode.FitScreen
+         })
         this.start(ResourceLoader).then(() => this.startGame())
     }
 
