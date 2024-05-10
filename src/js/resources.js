b70ada6c53e4e7db95fef80d4fe953bb0e261d29
KokoDoko <benni_jolink@hotmail.com>
2024-05-10 12:56:02 +0200

    improve resource loader

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,10 +1,16 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 
+// voeg hier jouw eigen resources toe
 const Resources = {
     Fish: new ImageSource('images/fish.png')
 }
-const ResourceLoader = new Loader([
-    Resources.Fish
-])
+
+
+
+
+const ResourceLoader = new Loader()
+for (let res of Object.values(Resources)) {
+    ResourceLoader.addResource(res)
+}
 
 export { Resources, ResourceLoader }
